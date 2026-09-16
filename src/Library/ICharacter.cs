5c4c6ee220ebb0eaa28f1111b952a91baf9b59b5
gutiburon <gutierrezadrian280408@gmail.com>
2026-09-16 09:24:22 -0300

    Sistema IsMagic

--- src/Library/ICharacter.cs
+++ src/Library/ICharacter.cs
@@ -4,14 +4,13 @@ public interface ICharacter
     int AttackValue { get; set; }
     int DefenseValue { get; set; }
     int Health { get; set; }
-
-    IList<IItemOffensive> OffensiveItems { get;}
-    IList<IItemDefensive> DefensiveItems { get;}
     
-    void AddOffensiveItem(IItemOffensive item);
-    void AddDefensiveItem(IItemDefensive item);
-    void RemoveOffensiveItem(IItemOffensive item);
-    void RemoveDefensiveItem(IItemDefensive item);
+    bool IsMagic { get;}
+
+    IList<IItem> Items { get;}
+    
+    void AddItem(IItem item);
+    void RemoveItem(IItem item);
 
 
     void RecieveAttack(int attackValue);
